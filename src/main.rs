a4b94e1ccb3453247457c26c144409bc897f1fb3
Altug Sahin <altugsahin@gmail.com>
2020-04-25 21:13:53 -0400

    parse args as struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+mod options;
+
+#[tokio::main]
+async fn main() -> Result<(), reqwest::Error> {
+    let args: Vec<String> = env::args().collect();
+    let cli_args = options::Arguments::new(&args);
+    let url = cli_args.unwrap().url;
+
+    let res = reqwest::get(&url).await?;
+    let body = res.text().await?;
+
+    println!("{}", body);
+
+    Ok(())
 }
