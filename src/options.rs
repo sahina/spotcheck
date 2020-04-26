75ea9db786eb1c367974d057f9ce201a6cd9007c
Altug Sahin <altugsahin@gmail.com>
2020-04-25 22:11:58 -0400

    format

--- src/options.rs
+++ src/options.rs
@@ -1,33 +1,33 @@
 #[derive(Debug)]
 pub struct Arguments {
-  pub flag: String,
-  pub url: String,
+    pub flag: String,
+    pub url: String,
 }
 
 impl Arguments {
-  pub fn new(args: &[String]) -> Result<Arguments, &str> {
-    return parse(args);
-  }
+    pub fn new(args: &[String]) -> Result<Arguments, &str> {
+        return parse(args);
+    }
 }
 
 fn parse(args: &[String]) -> Result<Arguments, &str> {
-  if args.len() == 2 {
-    return if args[1] == "--help" {
-      Ok(Arguments {
-        flag: "--help".to_string(),
-        url: "".to_string(),
-      })
-    } else {
-      Err("unknown flag")
+    if args.len() == 2 {
+        return if args[1] == "--help" {
+            Ok(Arguments {
+                flag: "--help".to_string(),
+                url: "".to_string(),
+            })
+        } else {
+            Err("unknown flag")
+        };
     }
-  }
 
-  // spotcheck --url https://www.amazon.com/gp/buy/shipoptionselect/handlers/display.html?hasWorkingJavascript=1
-  if args.len() == 3 && args[1] == "--url" {
-    return Ok(Arguments {
-      flag: args[1].clone(),
-      url: args[2].clone(),
-    });
-  }
-  return Err("unknown args...");
+    // spotcheck --url https://www.amazon.com/gp/buy/shipoptionselect/handlers/display.html?hasWorkingJavascript=1
+    if args.len() == 3 && args[1] == "--url" {
+        return Ok(Arguments {
+            flag: args[1].clone(),
+            url: args[2].clone(),
+        });
+    }
+    return Err("unknown args...");
 }
