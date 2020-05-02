a2ab71c30509a04ef0ea68bc9f9dea26deff9a0e
altug <altugsahin@gmail.com>
2020-05-01 23:44:36 -0400

    minor

--- src/parse.rs
+++ src/parse.rs
@@ -26,25 +26,6 @@ pub fn to_doc(html: &str) -> Ranking {
 
   let title = parse_title(&document);
 
-  // // loop all ranking elements
-  // for (index, node) in document.find(Class("zg_hrsr_item")).enumerate() {
-  //   // parse rank number
-  //   let rank = node.find(Class("zg_hrsr_rank")).next().unwrap().text();
-
-  //   // parse rank section
-  //   let group = node
-  //     .find(Class("zg_hrsr_ladder").descendant(Name("a")))
-  //     .next()
-  //     .unwrap()
-  //     .text();
-
-  //   let clean_group = group$
-  //     .replace("&amp;", "")
-  //     .replace("\n", "")
-  //     .replace(";", "");
-  //   let re = Regex::new(r"\s+").unwrap();
-  //   let after = re.replace_all(clean_group.as_str(), " ");
-
   Ranking {
     asin: "asin".to_string(),
     ranks: vec!["1".to_string()],
